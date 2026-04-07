21271782388adfedcffbd0d73840bca2eabba4d0
ANANTHI K <gpyananthi@gmail.com>
2026-04-07 22:51:31 +0530

    Added basic C programs

---
+++ add_multi.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,s,m;
+    scanf("%d %d %d",&a,&b,&c);
+    s=a+b+c;
+    printf("sum=%d\n",s);
+    m=a*b*c;
+    printf("multiplication=%d",m);
+    return 0;
+}
