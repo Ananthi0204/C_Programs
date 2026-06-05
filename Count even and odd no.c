e805cef3a0a5451d9bade09172b659a3e7cc8639
ANANTHI K <gpyananthi@gmail.com>
2026-06-05 21:08:28 +0530

    Add files via upload

---
+++ Count even and odd no.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int i,n,a[100],even=0,odd=0;
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	scanf("%d",&a[i]);
+	for(i=0;i<n;i++)
+	if(a[i]%2==0)
+	even++;
+	else
+	odd++;
+	printf("even=%d \n",even);
+	printf("odd=%d",odd);
+	return 0;
+}
